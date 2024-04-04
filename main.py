f14fc00d5e79223909aac1047a9600b7d0625630
Darnel Castore <dcastor@student.clayton.edu>
2024-04-03 23:14:43 -0400

    App 3: PDF generator- add footer; add multiple lines

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 
 df = pd.read_csv("topics.csv")
 for index,row in df.iterrows():
-    pdf.add_page()
+    for i in range(row["Pages"]):
 
-    pdf.set_font(family="Times", style="B", size=12)
-    pdf.cell(w=0, h=12, txt= row["Topic"], align="L", ln=1, border=1)
-    pdf.line(10,20,200,20)
+        pdf.add_page()
+
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=12, txt= row["Topic"], align="L", ln=1)
+
+        for y in range(20, 298, 10):
+            pdf.line(10,20,200,20)
+        pdf.line(10,20,200,20)
+
+        pdf.ln(250)
+        pdf.set_font(family="Times", style= "I", size=8)
+        pdf.set_text_color(180, 180,180)
+        pdf.cell(w=0, h=10,txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
